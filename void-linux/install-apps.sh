543c11caa9a35e1408bcb138fd03d19ec5bca735
Xie Yuheng <xyheme@gmail.com>
2026-09-16 05:30:10 +0800

    void-linux: 重构 install-apps.sh 为分组数组
    - 用 bash 数组按功能分组（base/shell/editor/vcs/...）
    - 仍以单次 xbps-install 事务安装
    - 加 shebang 与 set -euo pipefail，补充 -y

--- void-linux/install-apps.sh
+++ void-linux/install-apps.sh
@@ -1,35 +1,93 @@
-sudo xbps-install \
-    rsync fish-shell emacs-pgtk ranger xz \
-    git tig nodejs \
-    ntp NetworkManager \
-    mpv ffmpeg tree wget curl \
-    fzf \
-    clang valgrind cloc gdb libsanitizer libsanitizer-devel \
-    libX11-devel SDL2-devel \
-    proxychains-ng vim zsh ncdu \
-    noto-fonts-cjk noto-fonts-ttf noto-fonts-ttf-extra \
-    fastfetch htop btop \
-    brillo \
-    ugrep the_silver_searcher \
-    zathura zathura-djvu zathura-pdf-mupdf zathura-ps \
-    djview calibre \
-    goldendict \
-    amber onefetch \
-    xwayland-satellite \
-    alsa-utils \
-    pipewire wireplumber libspa-bluetooth alsa-pipewire \
-    pulseaudio-utils \
-    niri foot dbus \
-    xdg-desktop-portal xdg-desktop-portal-gtk \
-    xdg-desktop-portal-termfilechooser \
-    chromium firefox \
-    fcitx5 fcitx5-chinese-addons fcitx5-cloudpinyin fcitx5-configtool \
-    fcitx5-gtk \
-    fcitx5-gtk+2 \
-    fcitx5-gtk+3 \
-    fcitx5-gtk4 \
-    fcitx5-qt \
-    fcitx5-qt5 \
-    fcitx5-qt6 \
-    fcitx5-rime \
-    vscode
+#!/usr/bin/env bash
+set -euo pipefail
+
+base=(
+    rsync xz tree
+)
+
+shell=(
+    fish-shell zsh
+)
+
+editor=(
+    emacs-pgtk vim ranger vscode
+)
+
+vcs=(
+    git tig
+)
+
+lang=(
+    nodejs
+)
+
+network=(
+    ntp NetworkManager
+    wget curl proxychains-ng
+)
+
+cli=(
+    fzf ugrep the_silver_searcher
+    fastfetch htop btop ncdu
+    amber onefetch
+)
+
+dev=(
+    clang valgrind cloc gdb libsanitizer libsanitizer-devel
+    libX11-devel SDL2-devel
+)
+
+media=(
+    mpv ffmpeg
+)
+
+fonts=(
+    noto-fonts-cjk noto-fonts-ttf noto-fonts-ttf-extra
+)
+
+docs=(
+    zathura zathura-djvu zathura-pdf-mupdf zathura-ps
+    djview calibre
+    goldendict
+)
+
+desktop=(
+    niri foot dbus
+    xwayland-satellite brillo
+    xdg-desktop-portal xdg-desktop-portal-gtk
+    xdg-desktop-portal-termfilechooser
+)
+
+audio=(
+    alsa-utils
+    pipewire wireplumber libspa-bluetooth alsa-pipewire
+    pulseaudio-utils
+)
+
+browser=(
+    chromium firefox
+)
+
+input=(
+    fcitx5 fcitx5-chinese-addons fcitx5-cloudpinyin fcitx5-configtool
+    fcitx5-gtk fcitx5-gtk+2 fcitx5-gtk+3 fcitx5-gtk4
+    fcitx5-qt fcitx5-qt5 fcitx5-qt6
+    fcitx5-rime
+)
+
+sudo xbps-install -y \
+    "${base[@]}" \
+    "${shell[@]}" \
+    "${editor[@]}" \
+    "${vcs[@]}" \
+    "${lang[@]}" \
+    "${network[@]}" \
+    "${cli[@]}" \
+    "${dev[@]}" \
+    "${media[@]}" \
+    "${fonts[@]}" \
+    "${docs[@]}" \
+    "${desktop[@]}" \
+    "${audio[@]}" \
+    "${browser[@]}" \
+    "${input[@]}"
